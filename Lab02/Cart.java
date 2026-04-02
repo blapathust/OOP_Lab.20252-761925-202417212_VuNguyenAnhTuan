bf1423737757c3889bb9c523450cff3c2cb2b51a
Bl4p <blapthec4tu5@gmail.com>
2026-04-02 14:55:36 +0700

    added AIMS.java

---
+++ Lab02/Cart.java
@@ -0,0 +1,6 @@
+public class Cart {
+    
+    public static final int MAX_NUMBERS_ORDERED = 20;
+
+    private DVD itemsOrdered[] = new DVD[MAX_NUMBERS_ORDERED];
+}
---
+++ Lab02/DVD.java
@@ -0,0 +1,42 @@
+public class DVD {
+    
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public DVD(String name, String tag, String director_name, int runtime, float price)
+    {
+        this.title = name;
+        this.category = tag;
+        this.director = director_name;
+        this.length = runtime;
+        this.cost = price;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public String getDirector()
+    {
+        return director;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public float getCost()
+    {
+        return cost;
+    }
+}
