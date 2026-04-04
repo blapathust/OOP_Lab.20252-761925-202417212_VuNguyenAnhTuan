531290f716c00c388768075f1cd161c9be2e92a0
Bl4p <blapthec4tu5@gmail.com>
2026-04-04 19:05:45 +0700

    restored lab01 after accidental deletion

---
+++ Lab01/dialog_2_2_2.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JOptionPane;
+
+public class dialog_2_2_2 {
+    
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hello, World! How are you?");
+        System.exit(0);
+    }
+}
---
+++ Lab01/input_dialog_2_2_3.java
@@ -0,0 +1,7 @@
+public class input_dialog_2_2_3 {
+    public static void main(String[] args) {
+        String name = javax.swing.JOptionPane.showInputDialog("What is your name?");
+        javax.swing.JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
+        System.exit(0);
+    }
+}
