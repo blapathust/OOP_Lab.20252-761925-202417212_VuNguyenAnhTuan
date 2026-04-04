bf9ccd43cf59856a832532ea54b3786d2587f1cd
Vũ Nguyễn Anh Tuân <tuan.vna2417212@sis.hust.edu.vn>
2026-04-04 16:19:31 +0700

    Delete Lab01 directory

--- Lab01/hello_world_2_2_1.java
+++
@@ -1,6 +0,0 @@
-public class hello_world_2_2_1 {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban");
-        System.out.println("Hello \t World");
-    }
-}
