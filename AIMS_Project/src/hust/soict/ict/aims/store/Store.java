84d137e127f34d1b9a227e01263dba494e011431
Vu Nguyen Anh Tuan <blapthec4tu5@gmail.com>
2026-04-19 18:26:29 +0700

    .

---
+++ AIMS_Project/src/hust/soict/ict/aims/store/Store.java
@@ -0,0 +1,22 @@
+package hust.soict.ict.aims.store;
+
+import hust.soict.ict.aims.disc.DVD;
+
+import java.util.ArrayList;
+
+public class Store {
+
+    ArrayList<DVD> itemsInStore = new ArrayList<>();
+
+    public void addDVD(DVD dvd)
+    {
+        itemsInStore.add(dvd);
+        System.out.println("Added " + dvd.getTitle() + " to store.");
+    }
+
+    public void removeDVD(DVD dvd)
+    {
+        itemsInStore.remove(dvd);
+        System.out.println("Removed " + dvd.getTitle() + " from store.");
+    }
+}
