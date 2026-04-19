8c435b079c1ec99262b30c3b97e0c5595125d572
Vu Nguyen Anh Tuan <blapthec4tu5@gmail.com>
2026-04-19 18:34:22 +0700

    added store and store test classes

---
+++ AIMS_Project/src/hust/soict/ict/aims/store/StoreTest.java
@@ -0,0 +1,21 @@
+package hust.soict.ict.aims.store;
+
+import hust.soict.ict.aims.disc.DVD;
+
+public class StoreTest {
+
+    public static void main(String[] args) {
+        Store store = new Store();
+
+        DVD dvd1 = new DVD("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        store.addDVD(dvd1);
+
+        DVD dvd2 = new DVD("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        store.addDVD(dvd2);
+
+        DVD dvd3 = new DVD("Aladdin", "Animation", 18.99f);
+        store.addDVD(dvd3);
+
+        store.removeDVD(dvd2);
+    }
+}
