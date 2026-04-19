7eb6f3bfcc3f8790fcf11f69124d8d7704596023
Vu Nguyen Anh Tuan <blapthec4tu5@gmail.com>
2026-04-19 20:05:09 +0700

    done 6

---
+++ Other_Projects/hust/soict/ict/garbage/GarbageCreator.java
@@ -0,0 +1,22 @@
+package hust.soict.ict.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    
+    public static void main(String[] args) throws Exception {
+        String filename = "hust\\soict\\ict\\garbage\\test.exe";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        inputBytes = Files.readAllBytes(Paths.get(filename));
+        startTime = System.currentTimeMillis();
+        StringBuilder outputStringBuilder = new StringBuilder();
+        for (byte b : inputBytes) {
+        outputStringBuilder.append((char) b);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);    
+    }
+}
